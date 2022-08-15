c82c0ffdee1b21bfca6e892481b837b48ab21e29
HyperPaint <dioman172@yandex.ru>
2022-08-15 22:10:26 +1000

    Скрипт улучшения офиса

---
+++ Assets/Scripts/OfficeUpgrade.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+class OfficeUpgrade : MonoBehaviour
+{
+    public void OfficeInternetUpgradeClick()
+    {
+        MyGame.GameModel.Get().Offices[0].PartInternet.BuyUpgrade();
+    }
+
+    public void OfficeClimateUpgradeClick()
+    {
+        MyGame.GameModel.Get().Offices[0].PartClimate.BuyUpgrade();
+    }
+
+    public void OfficeMusicUpgradeClick()
+    {
+        MyGame.GameModel.Get().Offices[0].PartMusic.BuyUpgrade();
+    }
+}
+
